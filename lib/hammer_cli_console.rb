a881c638e3dc1729a4b16d590f367372c3cb9494
Martin Bačovský <martin.bacovsky@gmail.com>
2014-09-11 11:13:42 -0400

    new full-help command to recursively display the help text from all commands

--- lib/hammer_cli_console.rb
+++ lib/hammer_cli_console.rb
@@ -6,4 +6,8 @@ module HammerCLIConsole
                                          _("Launch a pry session in Hammer."),
                                          "HammerCLIConsole::ConsoleCommand",
                                          "hammer_cli_console/console")
+  HammerCLI::MainCommand.lazy_subcommand("full-help",
+                                         _("Recursive help of all hammer commands"),
+                                         "HammerCLIConsole::FullHelpCommand",
+                                         "hammer_cli_console/fullhelp")
 end
---
+++ lib/hammer_cli_console/fullhelp.rb
@@ -0,0 +1,32 @@
+module HammerCLIConsole
+
+  class FullHelpCommand < HammerCLI::AbstractCommand
+
+    def execute
+      puts HammerCLI::MainCommand.new('', {}).help
+      puts
+
+      print_help_recursive
+
+      HammerCLI::EX_OK
+    end
+
+    private
+
+    def print_help_recursive(command_name = 'hammer', command = HammerCLI::MainCommand)
+      command.recognised_subcommands.each do |subcommand_container|
+        subcommand_class = subcommand_container.subcommand_class
+        subcommand = subcommand_class.new("", {})
+        name = "#{command_name} > #{subcommand_container.names.first}"
+        puts name
+        puts '-' * name.length
+        puts subcommand_container.description
+        puts subcommand.help
+        puts
+        print_help_recursive name, subcommand_class
+      end
+    end
+
+  end
+
+end
